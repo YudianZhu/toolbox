b8bdf94f5223aa63252dbf613c92ddd35d5ae1db
jessie.zhu <zhuyudian@zhuyudiandeMacBook-Pro.local>
2016-10-11 11:29:34 +0800

    toolbox complete need review

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+requirejs.config({
+   paths:{
+       jquery:'jquery-3.1.0.min'
+   }
+});
+requirejs(['jquery','backtop'],function($,backtop){
+    $('#backTop').backtop({
+        mode:'move'
+    })
+});
